b715b1e6dedacf7546f904a44914ebfff89cd25b
Chibao78-code <ddinhchibao@gmail.com>
2025-08-11 21:33:39 +0700

    interface

---
+++ src/models/Functions.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package models;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author zzzdi
+ */
+public interface Functions {
+    boolean registerCustomer();
+    boolean updateCustomer();
+    List<Customer> searchCustomerByName();
+    void showFeastMenu();
+    boolean orderFeast();
+    boolean updateOrder();
+    boolean saveData();
+    void displayCustomerAndOrder();
+    void quitProgram() throws IOException, ClassNotFoundException;
+}
