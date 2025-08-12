a4456e1d2a4979b60b4a79b0ac412c1a42412053
Chibao78-code <ddinhchibao@gmail.com>
2025-08-12 08:01:00 +0700

    update

--- src/models/Order.java
+++ src/models/Order.java
@@ -18,7 +18,7 @@ public class Order implements Serializable {
     private String orderId;
     private String customerCode;
     private String setMenuCode;
-    private String evenDate;
+    private String eventDate;
     private int tableNumber;
     private double totalCost;
 
@@ -29,7 +29,7 @@ public class Order implements Serializable {
         this.orderId = orderId;
         this.customerCode = customerCode;
         this.setMenuCode = setMenuCode;
-        this.evenDate = evenDate;
+        this.eventDate = eventDate;
         this.tableNumber = tableNumber;
         this.totalCost = totalCost;
     }
@@ -58,12 +58,12 @@ public class Order implements Serializable {
         this.setMenuCode = setMenuCode;
     }
 
-    public String getEvenDate() {
-        return evenDate;
+    public String getEventDate() {
+        return eventDate;
     }
 
-    public void setEvenDate(String evenDate) {
-        this.evenDate = evenDate;
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
     }
 
     public int getTableNumber() {
@@ -84,8 +84,29 @@ public class Order implements Serializable {
 
     @Override
     public String toString() {
-        return "Order{" + "orderId=" + orderId + ", customerCode=" + customerCode + ", setMenuCode=" + setMenuCode + ", evenDate=" + evenDate + ", tableNumber=" + tableNumber + ", totalCost=" + totalCost + '}';
+        return "Order{" + "orderId=" + orderId + ", customerCode=" + customerCode + ", setMenuCode=" + setMenuCode + ", evenDate=" + eventDate + ", tableNumber=" + tableNumber + ", totalCost=" + totalCost + '}';
     }
     
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Order order = (Order) obj;
+        return orderId == order.orderId
+                && tableNumber == order.tableNumber
+                && Double.compare(order.totalCost, totalCost) == 0
+                && Objects.equals(customerCode, order.customerCode)
+                && Objects.equals(setMenuCode, order.setMenuCode)
+                && Objects.equals(eventDate, order.eventDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId, customerCode, setMenuCode, tableNumber, eventDate, totalCost);
+    }
     
 }
