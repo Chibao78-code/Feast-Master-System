84b8557a32777a626c4bbd64128674181c3b6436
Chibao78-code <ddinhchibao@gmail.com>
2025-08-12 14:49:20 +0700

    validcustomer

---
+++ src/validations/ValidCustomer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package validations;
+
+/**
+ *
+ * @author zzzdi
+ */
+public class ValidCustomer {
+    public static boolean validCode(String code) {
+        if (code.isEmpty()) {
+            return false;
+        }
+        
+        boolean isValid = false;
+        //Check valid code
+        String pattern = "^[CGK][0-9]{4}$";
+        if (code.matches(pattern)){
+            isValid = true;
+        }
+      
+}
