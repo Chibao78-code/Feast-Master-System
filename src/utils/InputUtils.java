291157361371eb8fbb77e8de95f59d8e9e575f4b
Chibao78-code <ddinhchibao@gmail.com>
2025-08-12 08:10:47 +0700

    InputUtils from user

---
+++ src/utils/InputUtils.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zzzdi
+ */
+public class InputUtils {
+    
+    public static int getInt(String text){
+        int number;
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.print(text);
+            number = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e){
+            number = -1;
+            
+            
+        }
+        return number;
+    }
+    
+    public static String getString(String text) {
+        String input = null;
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.print(text);
+            input = sc.nextLine();
+        } catch (Exception e){
+            System.out.println("Error while using getString():" + e.getLocalizedMessage());
+        }
+        return input;
+    }
+}
